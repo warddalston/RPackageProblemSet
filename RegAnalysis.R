e9f0f439875dbd111e3fe5aa78cf370aca2cb73c
warddalston <ward.dalston@gmail.com>
2014-03-07 13:13:56 -0600

    Basic functions clean up
    Edited the documentation and naming of things in my first functions.
    The intercept vector is now added IN the function.  Output objects have
    more informative names too.

--- RegAnalysis.R
+++ RegAnalysis.R
@@ -7,7 +7,7 @@
 #' \itemize{
 #' \item \code{coefficients} A matrix of the regression coefficients created by regressions of all possible combinations of \code{X} columns on \code{y}
 #' \item \code{R2} A vector of R^2 values from the regressions of all possible combinations of \code{X} on \code{y}
-#' \item \code{X} The first input, a matrix of covariate values
+#' \item \code{X} The first input, a matrix of covariate values (intercept vector not included)
 #' \item \code{y} the second input, a numeric vector of the same length as the number of rows of \code{X} 
 #' }
 #'
@@ -32,9 +32,21 @@ setClass(Class="RegAnalysis",
 
 #' @export
 setMethod("initialize", "RegAnalysis", 
-          function(.Object, ...){
-            value=callNextMethod()
-            return(value)
+          function(.Object, coefficients=matrix(0,nrow=1,ncol=1), R2=numeric(0),X=matrix(0,nrow=1,ncol=1),y=numeric(0)){
+            if(any(is.na(X))){
+              stop("The function does not accept covariate matrices with missing values")
+            }
+            if(any(is.na(y))){
+              stop("The function does not accept outcome vectors with missing values")
+            }
+            if(length(y)!=nrow(X)){
+              stop("The length of y and the number of rows of X must be equal")
+            }
+            .Object@coefficients <- coefficients
+            .Object@R2 <- R2
+            .Object@X <- X
+            .Object@y <- y
+            .Object
           }
 ) 
 
--- fitRegAnalysis.R
+++ fitRegAnalysis.R
@@ -2,11 +2,11 @@
 #'
 #' Regresses all possible combinations of a covariates on an outcome variable
 #'
-#' @param X A matrix or data frame object with no missing values
+#' @param X A numeric matrix object of covariates with no missing values (should not include the intercept vector)
 #' @param y A numeric object with the same number of elements as \code{X} has rows.
 #'
 #' @return An object of class RegAnalysis containing
-#'  \item{coefficients}{A n by (2^n)-1 matrix of coefficient values, where n is the number of columns in \code{X}.  Each column in this object represeents the output of a single model, each row represents a given variable in \code{X}.}
+#'  \item{coefficients}{A n by (2^n)-1 matrix of coefficient values, where n is the number of columns in \code{X} plus 1 (for the intercept).  Each column in this object represeents the output of a single model, each row represents a given variable in \code{X}.}
 #'  \item{R2}{A numeric vector of length (2^n)-1 of R^2 values, where n is the number of columns in \code{X}.}
 #'  \item{X}{The first object input} 
 #'  \item{y}{The second object input}
@@ -16,7 +16,6 @@
 #' 
 #' set.seed(1801)
 #' myX <- matrix(rpois(n=60,lambda=15),ncol=4)
-#' myX <- cbind(1,myX)
 #' myY <- sample(1:100,15,replace=TRUE) 
 #' fitRegAnalysis(myX, myY)
 #' @rdname fitRegAnalysis
@@ -30,6 +29,10 @@ setGeneric(name="fitRegAnalysis",
 #' @export
 setMethod(f="fitRegAnalysis",
           definition=function(X, y, ...){
+            
+            #before everything, add the intercept vector
+            X <- cbind(1,X)
+            
           #The method first creates a logical matrix specifying which variables are in each of the combinations.  There are (2^n)-1 possible combinations of covariates (including the intercept as a covariate).  The reason for the -1 is that it is not possible to fit a model with no intercepts and no covariates!  
           selectorList <- sapply(1:ncol(X),function(x){ 
               apply(combn(1:ncol(X),x), 2, function(z){
@@ -43,8 +46,10 @@ setMethod(f="fitRegAnalysis",
           #The next section creates the objects that will be in the output.  They have informative names and are sized based on parameter inputs.   
           Coefficients <- matrix(nrow=ncol(X),ncol=2^ncol(X)-1)
           R.squareds <- vector(length=2^ncol(X)-1)
-          colnames(Coefficients) <- names(R.squareds) <- apply(selectorMatrix,2,function(x){ paste("Vars:",paste(which(x==TRUE),collapse=","))})
-          rownames(Coefficients) <- paste("Variable", 1:5, sep=" ")
+          colnames(Coefficients) <- names(R.squareds) <- apply(selectorMatrix,2,function(x){
+            gsub("0","Int",paste("Vars:",paste(which(x==TRUE)-1,collapse=",")))
+            })
+          rownames(Coefficients) <- c("Int",paste("Variable", 1:(ncol(X)-1), sep=" "))
           
           #The final section of this function does the actual regression fitting.  Using a_ply it fits a regression for each column of the selectorMatrix where the covariates are the based on the "TRUE" values for a given column.  Then, it writes the coefficent values and R^2 value to the appropriate element of the output objects using "<<-" to force writing these to the first variable matching the specified name in a parent environment.  This is basically R-cheating.   
           a_ply(.data=1:ncol(selectorMatrix),.margins=1,.fun=function(z){
@@ -54,12 +59,6 @@ setMethod(f="fitRegAnalysis",
           })
           
           #it returns the output generated above as the input of the slots of an S4 object of "RegAnalysis" class
-          return(new("RegAnalysis", coefficients=Coefficients,R2=R.squareds,X=X,y=y))
+          return(new("RegAnalysis", coefficients=Coefficients,R2=R.squareds,X=X[,2:ncol(X)],y=y))
           }
 )
-
-set.seed(1801)
-myX <- matrix(rpois(n=60,lambda=15),ncol=4)
-myX <- cbind(1,myX)
-myY <- sample(1:100,15,replace=TRUE) 
-fitRegAnalysis(myX, myY)
