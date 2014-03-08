e260786717aa3b574b5ed3445d3733d1e31daa9f
warddalston <ward.dalston@gmail.com>
2014-03-08 17:38:47 -0600

    Documentation change
    This commit changes the documentation for printRegAnalysis.

--- printRegAnalysis.R
+++ printRegAnalysis.R
@@ -9,7 +9,7 @@
 #' @details The print method for objects of class `RegAnalysis' prints to the console the following information about a `RegAnalysis' object: the number of observations per regression, the number of regressions run, the number of covariates considered, the highest R^2 value, the variables included in that regression, the average R^2 value for each regressions including each covariate, the variance of R^2 values for each covariate, as well as the average value for each of the included coefficents.  
 #'
 #' @author Dalston G. Ward: \email{ward.dalston@gmail.com}
-#' @note The print and show methods for objects of class `RegAnalysis' do NOT return the same output.
+#' @note The print and show methods for objects of class `RegAnalysis' do NOT return the same output. Means and variances of R^2 rounded to four digits.  
 #' @seealso \code{\link{showRegAnalysis}}
 #' @rdname printRegAnalysis
 #' @export
